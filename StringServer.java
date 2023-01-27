57f69ea63c41a4241502fe4ed8838c6f28e48a2b
karinnamonzon <kmonzon@ucsd.edu>
2023-01-27 13:27:48 -0800

    Create StringServer.java

---
+++ StringServer.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    ArrayList<String> strList = new ArrayList<String>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.join("\n", strList);
+        } else if (url.getPath().equals("/search")) {
+            ArrayList<String> forReturn = new ArrayList<>();
+            String[] parameters = url.getQuery().split("=");
+                    for(String s : strList)
+                    {
+                        if(s.contains(parameters[1]))
+                        {
+                            forReturn.add(s);
+                        }
+                    }
+                
+                return String.join(", ", forReturn);
+        
+            } 
+            else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    strList.add(parameters[1]);
+                    return parameters[1] + " has been added!";
+                }
+            }
+            return "";
+        }
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
+
